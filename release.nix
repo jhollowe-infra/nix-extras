d199d30193568e95efd555853a7502ac81919625
John Hollowell <johnahollowell@gmail.com>
2024-08-11 02:11:38 +0000

    add release.nix

---
+++ release.nix
@@ -0,0 +1,20 @@
+{ nixpkgs ? <nixpkgs>, system ? builtins.currentSystem }:
+
+let
+  handleTest = t:
+    (import "${nixpkgs}/nixos/tests/make-test-python.nix") (import t);
+  pkgs = (import nixpkgs) {
+    overlays = [ (import ./default.nix) ];
+    config.allowUnfree = true;
+  };
+
+in rec {
+  # Evaluate overlay packages
+
+  # beegfs-client = pkgs.linuxPackages.beegfs;
+
+  # Tests
+  tests = {
+    # name = handleTest ./tests/name.nix {};
+  };
+}
